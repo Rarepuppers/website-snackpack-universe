9b5bad6054b6c14d745a84a4c9a682c46496e1ca
Rarepuppers <mark.nguyen.dev@outlook.com>
2026-08-27 22:13:55 +1000

    feat(site): add Atlas Quest FAQ and support page

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  * couple of shared scripts. So we precache the shell and let each game page
  * cache itself the first time it's visited.
  */
-const CACHE = "snackpack-arcade-v10";
+const CACHE = "snackpack-arcade-v11";
 
 const SHELL = [
   "/",
